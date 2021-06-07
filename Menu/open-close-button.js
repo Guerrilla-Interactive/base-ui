a17c13014e5ab7a68686abc5e7cc74c977051fa3
Suman Chapai <sumanchapai@gmail.com>
2021-06-07 14:09:44 +0545

    menu update

--- Menu/open-close-button.js
+++ Menu/open-close-button.js
@@ -1,6 +1,6 @@
 const OpenCloseButton = ({children, ...props}) => <button {...props}>{children} </button>
 
-const OpenButton = (props) => <span {...props}> Menu </span>
+const OpenButton = (props) => <span {...props}> Menu Open </span>
 const CloseButton = (props) => <span {...props}> X </span>
 
 export default OpenCloseButton
